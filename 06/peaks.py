467b0091b7a7856eb3be93b12f148dbce75d33dc
Michal Valenta <xvalent3@fi.muni.cz>
2018-11-06 16:55:20 +0100

    06 Final

--- 06/peaks.py
+++ 06/peaks.py
@@ -2,11 +2,35 @@ from scipy.io import wavfile
 from numpy.fft import rfft
 import numpy as np
 
-fs, data = wavfile.read('SineWave_440Hz.wav') #sampleRate, #data
-n = len(data) # 132300 number of samples
-a = n / fs #fs = 44100 a = 3000ms
+rate, audio = wavfile.read('06/SineWave_440Hz.wav')
 
+if len(audio.shape) == 2:
+    audio = np.mean(audio, axis=1)
 
-cn = np.abs(rfft(data))
+N = audio.shape[0]
+L = N / rate
+print(f'Audio length: {L:.2f} seconds')
 
+M = rate
+audio = audio[:(N // M) * M]
+slices = np.matrix(np.split(audio, M))
+print(f'Audio shape: {audio.shape}, Sliced audio shape: {slices.shape}')
+
+slices = slices.T
+spectrum = rfft(slices, axis=1)
+spectrum = np.abs(spectrum)
+
+peaks = []
+for window in spectrum:
+    avg = np.average(window)
+    for idx, value in enumerate(window):
+        if((avg * 20) < value):
+            peaks.append(idx)
+
+if(len(peaks) > 0):
+    a = np.amin(peaks)
+    b = np.amax(peaks)
+    print(f'low = {a}, high = {b}')
+else:
+    print('no peaks')
 
