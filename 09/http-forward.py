9086a39028d65e0abfb3e99c2cd3c28a71956bd3
Michal Valenta <xvalent3@fi.muni.cz>
2018-11-22 23:17:10 +0100

    09 - Added GET to forward

--- 09/http-forward.py
+++ 09/http-forward.py
@@ -1,9 +1,9 @@
 import http.server
-import http.client
+import urllib3
 import json
 
 PORT = 9999
-UPSTREAM = 'www.google.com'
+UPSTREAM = 'httpbin.org/ip'
 
 class MyHandler(http.server.BaseHTTPRequestHandler):
 
@@ -12,17 +12,24 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
         pass
 
     def do_GET(self):
-        connection = http.client.HTTPConnection(UPSTREAM, timeout=1)
-        print(connection)
-        
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
+        url = 'http://{}'.format(UPSTREAM)
+        try:
+            http = urllib3.PoolManager()
+            headers = {x[0]:x[1] for x in self.headers._headers if x[0] not in ('Connection','User-Agent', 'Host')}
+            r = http.request('GET', url, headers=headers)
+            self.send_response(r.status)
+            for key, value in r.headers._container.items():
+                self.send_header(*value)
+            self.end_headers()
+            self.wfile.write(r.data)
+        except:
+            pass
 
 
+server_address = ('127.0.0.1', PORT)
 
 try:
-    server = http.server.HTTPServer(('localhost', PORT), MyHandler)
+    server = http.server.HTTPServer(server_address, MyHandler)
     print('Started http server')
     server.serve_forever()
 except KeyboardInterrupt:
