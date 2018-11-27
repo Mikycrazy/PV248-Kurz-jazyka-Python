6fbbe62e2e6b90e29e6e75875633c8e87085fe30
Michal Valenta <xvalent3@fi.muni.cz>
2018-11-27 14:24:16 +0100

    09 - Almost Final

--- 09/http-forward.py
+++ 09/http-forward.py
@@ -2,19 +2,17 @@ import http.server
 import urllib3
 import json
 import urllib.request
-from urllib.parse import urlparse
+import socket
+from urllib.error import HTTPError, URLError
 
 PORT = 9999
-UPSTREAM = 'httpbin.org/headers'
+UPSTREAM = 'httpbin.org/ip'
 
 class MyHandler(http.server.BaseHTTPRequestHandler):
 
-    def writeFile(self, str):
-        self.wfile.write(str.encode("utf-8"))
-        pass
-
     def do_GET(self):
         url = 'http://{}'.format(UPSTREAM)
+        d={}
         try:    
             headers = {x[0]:x[1] for x in self.headers._headers if x[0] not in ('Host')}
             req = urllib.request.Request(url, headers=headers)
@@ -26,49 +24,60 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
                 d['json'] = json.loads(data.decode('utf8').replace("'", '"'))
             except ValueError:
                 d['content'] = data.decode('utf8')
-            
-            self.send_response(r.status)
+
+        except socket.timeout:
+            d = {'code': "timeout"}
+        except (URLError, HTTPError) as error:
+            d = {'code': "500", 'content': error.__str__()}
+        except Exception as ex:
+            print(ex)
+        finally:
+            self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
             message = json.dumps(d)
             self.wfile.write(message.encode())
-        except:
-            pass
     
     def do_POST(self):
-        try:
-            # Doesn't do anything with posted data
-            content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
-            post_data = self.rfile.read(content_length) # <--- Gets the data itself
-            my_json = json.loads(post_data.decode('utf8').replace("'", '"'))
-
-            headers = my_json['headers']
-            url = my_json['url']
-            request_type = my_json['type']
-            timeout = int(my_json['timeout']) if 'timeout' in my_json.keys() else 1
-            content = None
-            if request_type == 'POST':
-                content = my_json['content']
-
-
-            req = urllib.request.Request(url, headers=headers, data=content)
-            r = urllib.request.urlopen(req, timeout=timeout)
-
-            data = r.read()
-            d = {'code': r.status, 'headers': {k:v for k,v in r.headers._headers }}
-
+        
+            content_length = int(self.headers['Content-Length'])    # <--- Gets the size of data
+            post_data = self.rfile.read(content_length)             # <--- Gets the data itself
+            d={}
             try:
-                d['json'] = json.loads(data.decode('utf8').replace("'", '"'))
-            except ValueError:
-                d['content'] = data.decode('utf8')
+                my_json = json.loads(post_data.decode('utf8').replace("'", '"'))
+                url = my_json['url']
+                headers = my_json['headers'] if 'headers' in my_json.keys() else {}
+                timeout = int(my_json['timeout']) if 'timeout' in my_json.keys() else 1
+                request_type = my_json['type'] if 'type' in my_json.keys() else 'GET'
+                content = None
+                if request_type == 'POST':
+                    content = my_json['content'].encode('utf8')        
 
-            self.send_response(r.status)
-            self.send_header('Content-type', 'application/json')
-            self.end_headers()
-            message = json.dumps(d)
-            self.wfile.write(message.encode())
-        except:
-            pass
+                req = urllib.request.Request(url, headers=headers, data=content)
+                r = urllib.request.urlopen(req, timeout=timeout)
+                data = r.read()
+
+                d = {'code': r.status, 'headers': {k:v for k,v in r.headers._headers}}
+                try:
+                    d['json'] = json.loads(data.decode('utf8').replace("'", '"'))
+                except ValueError:
+                    d['content'] = data.decode('utf8')
+
+            except (ValueError, KeyError):
+                d = {'code': "invalid json" }
+            except socket.timeout:
+                d = {'code': "timeout"}
+            except (URLError, HTTPError) as error:
+                d = {'code': "500", 'content': error.__str__()}
+            except Exception as ex:
+                print(ex)
+            finally:
+                self.send_response(200)
+                self.send_header('Content-type', 'application/json')
+                self.end_headers()
+                message = json.dumps(d)
+                self.wfile.write(message.encode())
+
 
 
 
