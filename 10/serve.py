85a80460ff8d125bc9efd23f2a52c86fd46a85ad
Michal Valenta <xvalent3@fi.muni.cz>
2018-12-04 19:35:02 +0100

    10 - Final

--- 10/serve.py
+++ 10/serve.py
@@ -3,25 +3,108 @@ import asyncio
 import sys
 from aiohttp import web
 from aiohttp import streamer
+from concurrent.futures import ThreadPoolExecutor
 
+import re
+
+regex = r"\/(.+?)(\..+?)(\/.*)?$"
 DIR = '10/static'
+PORT = 15555
 
-async def get_date():
-    code = 'import datetime; print(datetime.datetime.now())'
+e = ThreadPoolExecutor()
+async def read_file(file_path):
+     loop = asyncio.get_event_loop()
+     with open(file_path) as f:
+        return (await loop.run_in_executor(e, f.read))
 
-    # Create the subprocess; redirect the standard output
-    # into a pipe.
+
+def get_file_name(path):
+    m = re.search(regex, path)
+    if m:
+        file_name = m.group(1)
+        file_extension = m.group(2)
+        path_info = m.group(3) if m.group(3) else ""
+
+    return file_name, file_extension, path_info
+
+async def run_cgi(file_path, ipt=None):
     proc = await asyncio.create_subprocess_exec(
-        sys.executable, '-c', code,
-        stdout=asyncio.subprocess.PIPE)
+        file_path,
+        stdout=asyncio.subprocess.PIPE,
+        stdin=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.STDOUT)
 
-    # Read one line of output.
-    data = await proc.stdout.readline()
-    line = data.decode('ascii').rstrip()
+    if ipt:
+        proc.stdin.write(ipt)
+        await proc.stdin.drain()
 
-    # Wait for the subprocess exit.
-    await proc.wait()
-    return line
+    stdout, _ = await proc.communicate()
+    return stdout.decode()
+
+def make_cgi_env(request, file_path, path_info):
+        """Set CGI environment variables"""
+
+        env = {}
+        env['SERVER_SOFTWARE'] = "09"
+        env['SERVER_NAME'] = "aiohttp"
+        env['GATEWAY_INTERFACE'] = 'CGI/1.1'
+        env['DOCUMENT_ROOT'] = DIR
+        env['SERVER_PROTOCOL'] = "HTTP/1.1"
+        env['SERVER_PORT'] = str(PORT)
+
+        env['REQUEST_METHOD'] = request.method
+        env['REQUEST_URI'] = str(request.raw_path)
+        env['PATH_TRANSLATED'] = DIR + path_info
+        env['REMOTE_HOST'] = request.host
+        env['SCRIPT_NAME'] = file_path
+        env['PATH_INFO'] = path_info
+        env['QUERY_STRING'] = request.query_string
+        env['CONTENT_LENGTH'] = str(request.content_length)
+        env['CONTENT_TYPE'] = str(request.content_type)
+        for k in request.headers:
+            env['HTTP_%s' %k.upper()] = request.headers[k]
+        os.environ.update(env)
+
+async def post_handle(request):
+    file_name, file_extension, path_info = get_file_name(request.path)
+    file_path = os.path.join(DIR, (file_name + file_extension))
+    if not os.path.exists(file_path):
+        return web.Response(
+            body='File <{file_name}> does not exist'.format(file_name=file_name),
+            status=404
+        )
+
+
+    data = await request.read()
+    
+    if file_extension == '.cgi':
+        make_cgi_env(request, file_path, path_info)
+        result = await run_cgi(file_path, data)
+        return web.Response(text=result)
+
+    
+
+
+async def get_handle(request):
+
+    file_name, file_extension, path_info = get_file_name(request.path)
+    file_path = os.path.join(DIR, (file_name + file_extension))
+    if not os.path.exists(file_path):
+        return web.Response(
+            body='File <{file_name}> does not exist'.format(file_name=file_name),
+            status=404
+        )
+
+    if file_extension == '.cgi':
+        make_cgi_env(request, file_path, path_info)
+        result = await run_cgi(file_path)
+        return web.Response(text=result)
+    
+    return web.Response(
+        body=file_sender(file_path=file_path),
+        headers={ "Content-disposition": "attachment; filename={file_name}".format(file_name=file_name) }
+    )
+
 
 
 @streamer
@@ -34,37 +117,14 @@ async def file_sender(writer, file_path=None):
         chunk = f.read(2 ** 16)
         while chunk:
             await writer.write(chunk)
-            chunk = f.read(2 ** 16)
-
-async def handle(request):
-    file_name = request.match_info.get('fileName')
-    _, file_extension = os.path.splitext(file_name)
-    if file_extension == '.cgi':
-        result = await get_date()
-        return web.Response(text=result)
-
-    headers = {
-        "Content-disposition": "attachment; filename={file_name}".format(file_name=file_name)
-    }
-
-    file_path = os.path.join(DIR, file_name)
-    if not os.path.exists(file_path):
-        return web.Response(
-            body='File <{file_name}> does not exist'.format(file_name=file_name),
-            status=404
-        )
-    
-    return web.Response(
-        body=file_sender(file_path=file_path),
-        headers=headers
-    )
+            chunk = f.read(2 ** 16) 
 
 if sys.platform == 'win32':
     loop = asyncio.ProactorEventLoop()
     asyncio.set_event_loop(loop)
 
 app = web.Application()
-app.add_routes([web.get('/', handle),
-                web.get('/{fileName}', handle)])
+app.add_routes([web.get('/{tail:.*}', get_handle),
+                web.post('/{tail:.*}', post_handle)])
 
 web.run_app(app, port=15555)
